563afebbd7b03962e3af711c714939afbb235ec7
Hadar Mahasri <hadarhadar690@gmail.com>
2025-12-29 19:06:42 +0200

    push

--- JS/game1.js
+++ JS/game1.js
@@ -196,10 +196,12 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function isColliding(rect1, rect2) {
-        return !(rect1.right < rect2.left || 
-                 rect1.left > rect2.right || 
-                 rect1.bottom < rect2.top || 
-                 rect1.top > rect2.bottom);
+        return (
+            rect1.left < rect2.right &&
+            rect1.right > rect2.left &&
+            rect1.top < rect2.bottom &&
+            rect1.bottom > rect2.top
+        );
     }
 
     // --- 8. ניהול מצב משחק ---
